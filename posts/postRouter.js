a6541fc45e203bf11a4ef875993a0d8f1e2849a4
Mosharraf Musa <mosharrafny@gmail.com>
2020-06-10 17:14:48 -0400

    Implement posts/postRouter endpoints middleware---some stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,48 @@
 const express = require("express");
 
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+  validatePostId,
+} = require("../middleware");
+
+const DB = require("./postDb");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   // do your magic!
+  DB.get()
+    .then((response) => res.status(200).json(response))
+    .catch((err) => res.status(500).json("sum ting wong"));
 });
 
-router.get("/:id", (req, res) => {
-  // do your magic!
+router.get("/:id", validatePostId, (req, res) => {
+  res.status(200).json(req.post);
 });
 
-router.delete("/:id", (req, res) => {
-  // do your magic!
+router.post("/", validatePost, async (req, res) => {
+  const post = await DB.insert(req.body);
+  if (post) {
+    return res.status(201).json(post);
+  }
+  res.status(500).json("Oof.");
 });
 
-router.put("/:id", (req, res) => {
-  // do your magic!
+router.delete("/:id", validatePostId, async (req, res) => {
+  const post = await DB.remove(req.post.id);
+  if (post > 0) {
+    return res.status(201).json(`${post} post has been removed`);
+  }
+  res.status(404).json({ message: "No post removed" });
 });
 
-// custom middleware
-
-function validatePostId(req, res, next) {
-  // do your magic!
-}
+router.put("/:id", validatePost, async (req, res) => {
+  const post = await DB.update(req.params.id, req.body);
+  if (post > 0) {
+    return res.status(201).json(`${post} post has been Updated`);
+  }
+  res.status(404).json({ message: "No post updated" });
+});
 
 module.exports = router;
