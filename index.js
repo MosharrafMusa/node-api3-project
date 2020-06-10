309156b6ae4e8309ec1a0b515c19a2c9b3bd928b
Mosharraf Musa <mosharrafny@gmail.com>
2020-06-10 16:51:06 -0400

    Users Middleware working----MVP

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+require("dotenv").config();
+
+const app = require("./server");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is up and listening at port: ${PORT}`);
+});
---
+++ middleware/index.js
@@ -0,0 +1,62 @@
+const postDB = require("../posts/postDb");
+
+const userDB = require("../users/userDb");
+
+function logger(req, res, next) {
+  console.log(
+    `request method: ${req.method}, request url: "${
+      req.url
+    }", request timestamp: ${Date()} `
+  );
+  next();
+}
+
+async function validateUserId(req, res, next) {
+  const found = await userDB.getById(req.params.id);
+  if (!found) {
+    return res.status(404).json({ message: "invalid user id" });
+  }
+  res.user = found;
+  next();
+}
+
+function validateUser(req, res, next) {
+  if (Object.keys(req.body).length > 0) {
+    if (req.body.name) {
+      return next();
+    }
+    return res.status(400).json({ message: "missing required name field" });
+  }
+  res.status(400).json({ message: "missing user data" });
+}
+
+function validatePost(req, res, next) {
+  if (Object.keys(req.body).length > 0) {
+    if (req.body.text && req.body.user_id) {
+      return next();
+    }
+    return res
+      .status(400)
+      .json({ message: "missing required text and user_id field" });
+  }
+  res.status(400).json({ message: "missing post data" });
+}
+
+async function validatePostId(req, res, next) {
+  const found = await postDB.getById(req.params.id);
+  if (found) {
+    req.post = found;
+    return next();
+  } else {
+    return res.status(404).json({ message: "Post is not found" });
+  }
+  res.status(500).json({ message: "uh oh!" });
+}
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+  validatePostId,
+};
